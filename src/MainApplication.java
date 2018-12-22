22e9ce31740688d62e81727937267df55ef1de52
rponly <rponly99@gmail.com>
2018-12-22 19:28:05 +0700

    add background to gui

--- src/MainApplication.java
+++ src/MainApplication.java
@@ -1,5 +1,11 @@
 import javax.swing.*;
-
+import java.awt.Dimension;
+import java.awt.Graphics;
+import java.awt.Image;
+//ww  w  .  j av a2s  . com
+import javax.swing.ImageIcon;
+import javax.swing.JFrame;
+import javax.swing.JPanel;
 public class MainApplication extends JFrame {
 
     public static void main(String[] args) { new MainApplication(); }
@@ -10,6 +16,36 @@ public class MainApplication extends JFrame {
         setResizable(false);
         setVisible(true);
         setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+        ImagePanel panel = new ImagePanel(
+                new ImageIcon("resource/titie.png").getImage());
+
+        JFrame frame = new JFrame();
+        frame.getContentPane().add(panel);
+        frame.pack();
+        frame.setVisible(true);
+    }
+
+}
+class ImagePanel extends JPanel {
+
+    private Image img;
+
+    public ImagePanel(String img) {
+        this(new ImageIcon(img).getImage());
+    }
+
+    public ImagePanel(Image img) {
+        this.img = img;
+        Dimension size = new Dimension(img.getWidth(null), img.getHeight(null));
+        setPreferredSize(size);
+        setMinimumSize(size);
+        setMaximumSize(size);
+        setSize(size);
+        setLayout(null);
+    }
+
+    public void paintComponent(Graphics g) {
+        g.drawImage(img, 0, 0, null);
     }
 
 }
