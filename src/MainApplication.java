cc5cf803f33cc69ea21d55a6142de10895c78d52
SMSHKR <39004788+SMSHKR@users.noreply.github.com>
2018-12-22 14:48:03 +0700

    Placeholder Main Window

---
+++ src/MainApplication.java
@@ -0,0 +1,15 @@
+import javax.swing.*;
+
+public class MainApplication extends JFrame {
+
+    public static void main(String[] args) { new MainApplication(); }
+
+    public MainApplication() {
+        setTitle("Placeholder");
+        setBounds(50, 50, 500, 500);
+        setResizable(false);
+        setVisible(true);
+        setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+    }
+
+}
