9a3ad7038dbec8a7eb4f756e84d043acd42e750e
SMSHKR <39004788+SMSHKR@users.noreply.github.com>
2018-12-22 19:53:25 +0700

    Add Title Screen

--- src/MainApplication.java
+++ src/MainApplication.java
@@ -1,51 +1,22 @@
 import javax.swing.*;
-import java.awt.Dimension;
-import java.awt.Graphics;
-import java.awt.Image;
-//ww  w  .  j av a2s  . com
-import javax.swing.ImageIcon;
 import javax.swing.JFrame;
-import javax.swing.JPanel;
+
 public class MainApplication extends JFrame {
 
     public static void main(String[] args) { new MainApplication(); }
 
     public MainApplication() {
+
         setTitle("Placeholder");
         setBounds(50, 50, 500, 500);
         setResizable(false);
         setVisible(true);
         setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
-        ImagePanel panel = new ImagePanel(
-                new ImageIcon("resource/titie.png").getImage());
 
-        JFrame frame = new JFrame();
-        frame.getContentPane().add(panel);
-        frame.pack();
-        frame.setVisible(true);
-    }
+        ImagePanel panel = new ImagePanel("resource/title.png");
+        add(panel);
+        pack();
 
-}
-class ImagePanel extends JPanel {
-
-    private Image img;
-
-    public ImagePanel(String img) {
-        this(new ImageIcon(img).getImage());
-    }
-
-    public ImagePanel(Image img) {
-        this.img = img;
-        Dimension size = new Dimension(img.getWidth(null), img.getHeight(null));
-        setPreferredSize(size);
-        setMinimumSize(size);
-        setMaximumSize(size);
-        setSize(size);
-        setLayout(null);
-    }
-
-    public void paintComponent(Graphics g) {
-        g.drawImage(img, 0, 0, null);
     }
 
 }
