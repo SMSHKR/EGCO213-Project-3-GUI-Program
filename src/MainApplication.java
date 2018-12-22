0f0dc707ce9e1ff1b7cf0f2de32dbdd340ee04fe
SMSHKR <39004788+SMSHKR@users.noreply.github.com>
2018-12-22 20:46:01 +0700

    Add playButton()

--- src/MainApplication.java
+++ src/MainApplication.java
@@ -8,9 +8,10 @@ public class MainApplication extends JFrame {
     public MainApplication() {
 
         setTitle("Placeholder");
-        setBounds(50, 50, 500, 500);
+        setLocation(10, 10);
         setResizable(false);
         setVisible(true);
+        //setLayout(null);
         setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
 
         titleScreen();
@@ -24,14 +25,29 @@ public class MainApplication extends JFrame {
         pack();
 
         // TODO rabbit animation
-        try { Thread.sleep(3000); }
+        try { Thread.sleep(1000); }
         catch (InterruptedException e) { }
 
         remove(panel);
         panel = new ImagePanel("resource/title_house.png");
+        panel.add(playButton());
         add(panel);
         pack();
 
+    }
+
+    private JButton playButton() {
+
+        JButton playButton = new JButton();
+        ImageIcon playIcon = new ImageIcon("resource/play_normal.png");
+
+        playButton.setBounds(100, 100, playIcon.getIconWidth(), playIcon.getIconHeight());
+        playButton.setIcon(playIcon);
+        playButton.setBorderPainted(false);
+        playButton.setFocusPainted(false);
+        playButton.setContentAreaFilled(false);
+
+        return playButton;
     }
 
 }
