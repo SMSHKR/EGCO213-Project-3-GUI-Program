9b605f6b5b2880df32c784ddeb77a49bf8750c9d
SMSHKR <39004788+SMSHKR@users.noreply.github.com>
2018-12-23 08:02:19 +0700

    Cleanup code

--- src/Screen1.java
+++ src/Screen1.java
@@ -1,12 +1,10 @@
 import javax.swing.*;
 import java.awt.*;
-import java.awt.event.WindowAdapter;
-import java.awt.event.WindowEvent;
 
-public class Screen1 extends JFrame
-{
-    public Screen1()
-    {
+public class Screen1 extends JFrame {
+
+    public Screen1() {
+
         JButton BackpackButton = new JButton();
         JPanel panel = new JPanel();
         setTitle("Location Name 1");
@@ -14,19 +12,20 @@ public class Screen1 extends JFrame
         setLocation(10, 10);
         setResizable(false);
         UI ui = new UI();
-        BackpackButton.setIcon(ui.backpack);
-        BackpackButton.setBounds(200,200,50,50);
-        setBounds(0,500,100,50);
+        BackpackButton.setIcon(ui.getBackpack());
+        BackpackButton.setBounds(200, 200, 50, 50);
+        setBounds(0, 500, 100, 50);
         panel.add(BackpackButton);
-        panel.setBounds(800,200,50,50);
+        panel.setBounds(800, 200, 50, 50);
         add(panel);
-        setSize(1920,1080);
+        setSize(1920, 1080);
         setVisible(true);
 
     }
-    public static void main(String[] args)
-    {
+
+    public static void main(String[] args) {
         new Screen1();
     }
+
 }
 
--- src/UI.java
+++ src/UI.java
@@ -1,12 +1,13 @@
 import javax.swing.*;
 
-public class UI extends JPanel
-{
-    public ImageIcon backpack,inventory;
-    public UI()
-    {
-        backpack = new ImageIcon("resource/backpack.png");
-        inventory = new ImageIcon("resource/slot_inventory.png");
-    }
+public class UI extends JPanel {
+
+    private ImageIcon backpack = new ImageIcon("resource/backpack.png");
+    private ImageIcon inventory = new ImageIcon("resource/slot_inventory.png");
+
+    public UI() { }
+
+    public ImageIcon getBackpack() { return backpack; }
+    public ImageIcon getInventory() { return inventory; }
 
 }
