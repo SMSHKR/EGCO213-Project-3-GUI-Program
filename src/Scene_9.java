cd3cb12c1909666618af97df4976dcaadc6eb05c
rponly <rponly99@gmail.com>
2018-12-25 04:52:00 +0700

    scene 9 need fix

--- src/Scene_9.java
+++ src/Scene_9.java
@@ -1,35 +1,22 @@
 class Scene_9 extends Scene {
     private boolean bad=true;
-    private MySoundEffect song1,song2;
+
+
     Scene_9(Game controller) {
 
         super("9", controller, 704, 302);
 
         background.add(rightArrowButton(8, true));
-
+        int mistake=0;
         for (int i = 0; i < 5; i++) {
             if (false == controller.getCorrect(i)) {
                 bad = false;
+                mistake++;
             }
         }
 
+        if(mistake>2)
+        if(mistake<3)background.add(leftArrowButton(10, true));
     }
 
-}
-class MySoundEffect
-{
-    private java.applet.AudioClip audio;
-
-    public MySoundEffect(String filename)
-    {
-        try
-        {
-            java.io.File file = new java.io.File(filename);
-            audio = java.applet.Applet.newAudioClip(file.toURL());
-        }
-        catch (Exception e) { e.printStackTrace(); }
-    }
-    public void playOnce()   { audio.play(); }
-    public void playLoop()   { audio.loop(); }
-    public void stop()       { audio.stop(); }
 }
