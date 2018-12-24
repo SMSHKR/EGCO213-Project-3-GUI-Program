bd688a21b3244838fe5f6ac593ce2333194477b1
rponly <rponly99@gmail.com>
2018-12-24 23:40:50 +0700

    remove scene10

--- src/Scene_5.java
+++ src/Scene_5.java
@@ -2,7 +2,7 @@ class Scene_5 extends Scene {
 
     Scene_5(Game controller) {
 
-        super("5", controller, 0, 0);
+        super("5", controller, 728, 804);
 
         background.add(rightArrowButton(3, true));
         background.add(leftArrowButton(6, true));
